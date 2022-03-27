e03194fc066c21b95c9251d9267687111d2b6570
“Joe” <51638822+tso996@users.noreply.github.com>
2022-03-27 20:11:44 +0100

    opencv import is causing the error at the end for some reason. Even edge detection does not build if we include opencv in the header part

--- mediapipe/examples/wasm_stuff/edge_detection_base/hello_world.cc
+++ mediapipe/examples/wasm_stuff/edge_detection_base/hello_world.cc
@@ -13,61 +13,9 @@
 // limitations under the License.
 //
 // A simple example to print out "Hello World!" from a MediaPipe graph.
-
-#include "mediapipe/framework/calculator_graph.h"
-#include "mediapipe/framework/port/logging.h"
-#include "mediapipe/framework/port/parse_text_proto.h"
-#include "mediapipe/framework/port/status.h"
-
-namespace mediapipe {
-
-absl::Status PrintHelloWorld() {
-  // Configures a simple graph, which concatenates 2 PassThroughCalculators.
-  CalculatorGraphConfig config =
-      ParseTextProtoOrDie<CalculatorGraphConfig>(R"pb(
-        input_stream: "in"
-        output_stream: "out"
-        node {
-          calculator: "PassThroughCalculator"
-          input_stream: "in"
-          output_stream: "out1"
-        }
-        node {
-          calculator: "PassThroughCalculator"
-          input_stream: "out1"
-          output_stream: "out"
-        }
-      )pb");
-
-  CalculatorGraph graph;
-  MP_RETURN_IF_ERROR(graph.Initialize(config));
-  ASSIGN_OR_RETURN(OutputStreamPoller poller,
-                   graph.AddOutputStreamPoller("out"));
-  MP_RETURN_IF_ERROR(graph.StartRun({}));
-  // Give 10 input packets that contains the same std::string "Hello World!".
-  for (int i = 0; i < 10; ++i) {
-    MP_RETURN_IF_ERROR(graph.AddPacketToInputStream(
-        "in", MakePacket<std::string>("Hello World!").At(Timestamp(i))));
-  }
-  // Close the input stream "in".
-  MP_RETURN_IF_ERROR(graph.CloseInputStream("in"));
-  mediapipe::Packet packet;
-  // Get the output packets std::string.
-  while (poller.Next(&packet)) {
-    LOG(INFO) << packet.Get<std::string>();
-  }
-  return graph.WaitUntilDone();
-}
-}  // namespace mediapipe
+// #include <opencv2/core/version.hpp>
 
 int main(int argc, char** argv) {
-  
-  std::cout << "This is edge detection!!!!!!!!!" << std::endl;
   return 0;
 }
-
-// int main(int argc, char** argv) {
-//   std::cout << "hello world!!!!!!!!!" << std::endl;
-//   return 0;
-// }
 
